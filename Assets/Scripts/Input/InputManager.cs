f86dc36c46ea33b96ccacf51a3eb556cd3a3c8f4
DataConstrictor <noel.kva@gmail.com>
2025-09-17 19:57:18 +0200

    Added interaction
     Added following behaviours:
    - Swapping between items
    - Putting down an item
    - Picking up an item
    -Scrolling between different items in inventory

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -4,16 +4,39 @@ public class InputManager : MonoBehaviour
 {
     private PlayerInput player_input;
     private PlayerInput.On_FootActions on_foot;
+    private PlayerInput.InventoryActions inventoryActions;
+
     private PlayerMovement player_movement;
     private PlayerLook player_look;
+    private InteractionManager interactionManager;
+    private int currentScrollIndex = 0;
+
 
     void Awake()
     {
         player_input = new PlayerInput();
         player_movement = GetComponent<PlayerMovement>();
         player_look = GetComponent<PlayerLook>();
+        interactionManager = GetComponent<InteractionManager>();
         on_foot = player_input.On_Foot;
+        inventoryActions = player_input.Inventory;
         on_foot.Jump.performed += ctx => player_movement.Jump();
+        on_foot.Interact.performed += ctx => interactionManager.OnInteractWithItem();
+        inventoryActions.Item1.performed += ctx => interactionManager.GetItemInInventory(0);
+        inventoryActions.Item2.performed += ctx => interactionManager.GetItemInInventory(1);
+        inventoryActions.Item3.performed += ctx => interactionManager.GetItemInInventory(2);
+        inventoryActions.Scroll.performed += ctx =>
+        {
+            float scroll = ctx.ReadValue<float>();
+
+            if (scroll > 0f) currentScrollIndex--; 
+            else if (scroll < 0f) currentScrollIndex++; 
+
+            if (currentScrollIndex < 0) currentScrollIndex = 2;
+            if (currentScrollIndex > 2) currentScrollIndex = 0;
+
+            interactionManager.GetItemInInventory(currentScrollIndex);
+        };
     }
 
     private void FixedUpdate()
@@ -29,10 +52,12 @@ public class InputManager : MonoBehaviour
     void OnEnable()
     {
         on_foot.Enable();
+        inventoryActions.Enable();
     }
 
     void OnDisable()
     {
         on_foot.Disable();
+        inventoryActions.Enable();
     }
 }
