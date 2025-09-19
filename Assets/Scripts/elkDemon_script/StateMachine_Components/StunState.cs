0c4a6c344410adf9fe044ea1afbfdad69de6ed00
Sakyey <gustaf.m.dahlqvist@gmail.com>
2025-09-19 12:03:24 +0200

    ?

---
+++ Assets/Scripts/elkDemon_script/StateMachine_Components/StunState.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class StunState : StateMachineBehaviour
+{
+    [Header("Stun Settings")]
+    [SerializeField] private float stunDuration = 3f;
+    [SerializeField] private float stunTimer = 0f;
+
+    private ElkDemonAI _elkDemon;
+    private bool _isStun = false;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_elkDemon == null)
+        {
+            _elkDemon = animator.GetComponent<ElkDemonAI>();
+        }
+
+        animator.ResetTrigger("Attack");
+        animator.SetBool("IsAttacking", false);
+
+        _elkDemon.StopMoving();
+        stunTimer = 0f;
+
+        _isStun = true;
+        animator.SetBool("IsStun", true);
+        Debug.Log("Entered Stun state!");
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_elkDemon == null || _elkDemon.Player == null) return;
+
+        stunTimer += Time.deltaTime;
+
+        if (stunTimer >= stunDuration)
+        {
+            animator.SetTrigger("StunComplete");
+            animator.SetBool("IsStun", false);
+            stunTimer = 0f;
+        }
+
+        
+    }
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("IsStun", false);
+        animator.ResetTrigger("StunComplete");
+        Debug.Log("Exited Stun State");
+    }
+}
