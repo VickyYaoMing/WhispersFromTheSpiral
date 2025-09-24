fd042eac2f50612618ff464ef93e95fe039fcfec
Ibrahim <ibrra187@gmail.com>
2025-09-25 00:13:36 +0200

    Comment

--- Assets/Scripts/ItemInteraction/Default_Item.cs
+++ Assets/Scripts/ItemInteraction/Default_Item.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Default_Item : InteractableBase
 {
+    //Saving and loading like this only works for one (1) object. Consider moving everything from here into the ItemManager, and the ItemSaveData struct holding a list of both objects and vectors. Maybe tuples.
     public void Save(ref ItemSaveData data)
     {
         data.itemPosition = transform.position;
