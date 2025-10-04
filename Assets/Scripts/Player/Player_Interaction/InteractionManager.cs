d82d3fa6421aabdddd4e0e50eb16218632c1b4ba
DataConstrictor <noel.kva@gmail.com>
2025-10-04 17:05:24 +0200

    Bug fixes

--- Assets/Scripts/Player/Player_Interaction/InteractionManager.cs
+++ Assets/Scripts/Player/Player_Interaction/InteractionManager.cs
@@ -115,6 +115,7 @@ public class InteractionManager : MonoBehaviour
 
     private void OnItemCameraLock()
     {
+        itemArray[currentItemSpot].SetActive(false);
         lockItem = true;
         inputManager.enabled = false;
         InteractableBase interactBase = currentItem.GetComponent<InteractableBase>();
@@ -138,6 +139,7 @@ public class InteractionManager : MonoBehaviour
             currentItem.GetComponent<InteractableBase>().enabled = false;
             lockItem = false;
             currentItem = itemArray[currentItemSpot];
+            currentItem.SetActive(true);
         }
     }
 
