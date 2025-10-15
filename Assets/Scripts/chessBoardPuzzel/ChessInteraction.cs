c6b2a223000b7aa4d69ff2f8738bef328bc35dfd
Karincjansson <Karin.c.jansson@outlook.com>
2025-10-15 15:13:24 +0200

    Changes on changes

--- Assets/Scripts/chessBoardPuzzel/ChessInteraction.cs
+++ Assets/Scripts/chessBoardPuzzel/ChessInteraction.cs
@@ -3,23 +3,24 @@ using UnityEngine;
 public class ChessInteraction : InteractableBase
 {
     private LayerMask pieceMask;
-    private LayerMask boardMask;
+    //private LayerMask boardMask;
 
     void Start()
     {
         itemShouldBeCameraLocked = true;
-        //pieceMask = StringLiterals.CHESS_PIECE_LAYER;
+        pieceMask = LayerMask.NameToLayer(StringLiterals.CHESS_PIECE_LAYER);
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("click");
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, 20f))
+            if (Physics.Raycast(ray, out hit, 20f, pieceMask))
             {
                 Debug.Log(hit.collider.name);
             }
