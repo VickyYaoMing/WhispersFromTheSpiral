6fc79ad1e964be5aa76a2a6ce92d0f3de6b910e4
EeeHello <thitiwich33@gmail.com>
2025-10-21 13:13:03 +0200

    Filler push (DoorInteraction stuff incomplete)

--- Assets/Scripts/elkDemon_script/StateMachine_Components/OpenDoorState.cs
+++ Assets/Scripts/elkDemon_script/StateMachine_Components/OpenDoorState.cs
@@ -1,6 +1,41 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class OpenDoorState
+
+public class OpenDoorState : MonoBehaviour
 {
-    
+    private NavMeshAgent _navAgent;
+    private bool openingDoor;
+
+    private void Awake()
+    {
+        _navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (openingDoor) return;
+
+        SmartDoorInteraction door = other.GetComponent<SmartDoorInteraction>();
+        if (door != null)
+        {
+            openingDoor = true;
+            _navAgent.isStopped = true;
+
+            Vector3 lookPos = other.transform.position - transform.position;
+            lookPos.y = 0;
+            transform.rotation = Quaternion.LookRotation(lookPos);
+
+            door.TryOpen(transform, true);
+
+            Invoke(nameof(ResumeMovement), 2.5f);
+        }
+    }
+
+    private void ResumeMovement()
+    {
+        _navAgent.isStopped = false;
+        openingDoor = false;
+    }
 }
