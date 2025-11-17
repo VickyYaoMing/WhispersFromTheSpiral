0d57dbf3dc8a744472c7d2833ec0706fd02802a7
DataConstrictor <noel.kva@gmail.com>
2025-11-17 13:43:58 +0100

    Fixed a bit

--- Assets/Scripts/ItemInteraction/SpecialInteractables/SafeInteraction.cs
+++ Assets/Scripts/ItemInteraction/SpecialInteractables/SafeInteraction.cs
@@ -1,7 +1,9 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SafeInteraction : InteractableBase
@@ -9,40 +11,47 @@ public class SafeInteraction : InteractableBase
     [SerializeField] private List<GameObject> anchors;
     [SerializeField] private Animator animator;
     [SerializeField] private LayerMask keypadMask;
-    private int[] correctCode = new int[] { 1, 9, 6, 9 };
+    [SerializeField] private int[] correctCode;
+    [SerializeField] private GameObject returnGameObject;
+    [SerializeField] private LayerMask interactMask;
+    [SerializeField] private float rayHitDistance = 100f;
+    private bool safeOpened = false;
+    private InteractionManager interactionManager;
 
     private int[] code;
     private int currentCodeIndex = 0;
 
     private void Start()
     {
+        interactionManager = GameManager.Instance.InteractionManager;
         itemShouldBeCameraLocked = true;
         code = new int[4];
+        returnGameObject.SetActive(false);
     }
    
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !safeOpened)
         {
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, 20f, keypadMask))
+            if (Physics.Raycast(ray, out hit, rayHitDistance, keypadMask))
             {
                 GameObject hitGO = hit.collider.gameObject;
-                
-                for(int i = 0; i < anchors.Count; i++)
+
+                for (int i = 0; i < anchors.Count; i++)
                 {
-                    if(anchors[i] == hitGO)
+                    if (anchors[i] == hitGO)
                     {
-                        if (hitGO.name == "Minus" )
+                        if (hitGO.name == "Minus")
                         {
                             animator.SetTrigger("Press" + hitGO.name);
                             ResetCode();
 
                         }
-                        else if(hitGO.name == "Plus")
+                        else if (hitGO.name == "Plus")
                         {
                             animator.SetTrigger("Press" + hitGO.name);
                             EnterCode();
@@ -52,15 +61,32 @@ public class SafeInteraction : InteractableBase
                             animator.SetTrigger("Press" + hitGO.name.Replace("No", ""));
                             code[currentCodeIndex] = int.Parse(hitGO.name.Replace("No", ""));
                             currentCodeIndex++;
-                            if(currentCodeIndex >= code.Length) currentCodeIndex = 0;
+                            if (currentCodeIndex >= code.Length) currentCodeIndex = 0;
                         }
                     }
                 }
 
             }
+        }
+        else if (Input.GetMouseButtonDown(0) && safeOpened)
+        {
+            Ray rayItem = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitItem;
 
+
+            if (Physics.Raycast(rayItem, out hitItem, rayHitDistance, interactMask))
+            {
+                if (hitItem.collider.gameObject.CompareTag("ItemInsideSafe"))
+                {
+                    returnGameObject.SetActive(false);
+                    interactionManager.OnPickUp(returnGameObject);
+                    interactionManager.ReleaseCameraLock();
+                }
+            }
         }
     }
+
+
 
     private void ResetCode()
     {
@@ -73,6 +99,8 @@ public class SafeInteraction : InteractableBase
         {
             animator.SetBool("IsNumActive", false);
             animator.SetTrigger("OpenSafe");
+            safeOpened = true;
+            returnGameObject.SetActive(true);
         }
         else
         {
