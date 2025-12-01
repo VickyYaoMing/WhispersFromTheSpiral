418f92ca6855c0b8d490b464950cfd30f1f2022a
HecticSwede <hectorastr@gmail.com>
2025-12-01 12:46:24 +0100

    Revert "Merge remote-tracking branch 'origin/AI-Rework' into DemoScene"
    This reverts commit 0403c24e623531ec84195de5bdf7533fe7c4d650, reversing
    changes made to 4954033e0a457bfdd915a2a3c2951ff83648d9ed.

--- Assets/Scripts/Menu/MainMenuNavigation.cs
+++ Assets/Scripts/Menu/MainMenuNavigation.cs
@@ -36,20 +36,21 @@ public class MainMenuNavigation : MonoBehaviour
     {
         m_loadButton.SetActive(false);
         m_startButton.transform.localPosition = m_defaultStartButtonPos;
-        if (GameManager.Instance.SaveSystem != null)
+        if (GameManager.Instance.saveExists)
         {
-            if (GameManager.Instance.DoesSaveExist())
-            {
-                m_loadButton.SetActive(true);
-                m_startButtonPosIfSaveExists = m_defaultStartButtonPos + new Vector3(0, -90, 0);
-                m_startButton.transform.localPosition = m_startButtonPosIfSaveExists;
-            }
-        }    
+            m_loadButton.SetActive(true);
+            m_startButtonPosIfSaveExists = m_defaultStartButtonPos + new Vector3(0, 90, 0);
+            m_startButton.transform.localPosition = m_startButtonPosIfSaveExists;
+        }
         m_camTransform = m_cameraObject.transform;
         m_initialPosition = m_cameraObject.transform.position;
         m_fadeAnimator = GetComponent<FadeAnimator>();
         m_optionsCanvasGroup.alpha = 0f;
         m_creditsCanvasGroup.alpha = 0f;
+        m_optionsCanvasGroup.interactable = false;
+        m_creditsCanvasGroup.interactable = false;
+        m_optionsCanvasGroup.blocksRaycasts = false;
+        m_creditsCanvasGroup.blocksRaycasts = false;
         m_fadeAnimator.FadeIn(m_mainCanvasGroup, 2f);
     }
     #endregion
@@ -58,6 +59,8 @@ public class MainMenuNavigation : MonoBehaviour
     {
         m_optionsCanvasGroup.interactable = false;
         m_creditsCanvasGroup.interactable = false;
+        m_optionsCanvasGroup.blocksRaycasts = false;
+        m_creditsCanvasGroup.blocksRaycasts = false;
 
         switch (m_currentState)
         {
@@ -71,6 +74,7 @@ public class MainMenuNavigation : MonoBehaviour
         m_fadeAnimator.FadeIn(m_mainCanvasGroup, 0.5f);
         m_currentState = MenuState.MainView;
         m_mainCanvasGroup.interactable = true;
+        m_mainCanvasGroup.blocksRaycasts = true;
     }
 
     public void StartGame()
@@ -81,6 +85,7 @@ public class MainMenuNavigation : MonoBehaviour
     public void LoadGame()
     {
         //Implement logic for loading save and necessary scene + data
+        GameManager.Instance.LoadAsync();
     }
 
     public void ViewOptions()
@@ -129,6 +134,6 @@ public class MainMenuNavigation : MonoBehaviour
         }
 
         m_camTransform.position = targetPosition;
-        SceneManager.LoadScene("Mansion_Main");
+        SceneManager.LoadScene("DemoSceneAct1");
     }
 }
