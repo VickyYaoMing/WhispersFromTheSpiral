b47a505d19effd38dd2defb2e152cd4d259ee54c
HecticSwede <hectorastr@gmail.com>
2025-12-02 22:33:42 +0100

    Implemented Credits Scene
    - Added scrolling credits
    - Implemented pressing Escape to skip the credits
    - Added sound to the credits screen, continues into main menu

--- Assets/Scripts/Menu/CreditsScreen.cs
+++ Assets/Scripts/Menu/CreditsScreen.cs
@@ -1,16 +1,56 @@
+using Assets.Scripts.AudioSystem;
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreditsScreen : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject creditsObject;
+
+    private CanvasGroup m_creditsGroup;
+    private FadeAnimator m_fadeAnimator;
+    private readonly float scrollSpeed = 100f;     
+    private readonly float scrollDistance = 5000f;
+    private Vector3 startPos;
+
     void Start()
     {
-        
+        m_creditsGroup = GetComponent<CanvasGroup>();
+        m_fadeAnimator = GetComponent<FadeAnimator>();
+
+        m_creditsGroup.alpha = 0.0f;
+        startPos = creditsObject.transform.localPosition;
+
+        SoundManager.PlayMusic(SoundType.UI_MenuTheme, 0f);
+        StartCoroutine(ScrollCredits());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ScrollCredits()
     {
-        
+        m_fadeAnimator.FadeIn(m_creditsGroup, 1f);
+
+        yield return new WaitForSeconds(2f);
+
+        float totalDistanceMoved = 0f;
+
+        while (totalDistanceMoved < scrollDistance)
+        {
+            if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                ReturnToMainMenu();
+            }
+            float amountToMove = scrollSpeed * Time.deltaTime;
+            totalDistanceMoved += amountToMove;
+
+            creditsObject.transform.localPosition += new Vector3(0, amountToMove, 0);
+            yield return null;
+        }
+        ReturnToMainMenu();
+    }
+
+    private void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("System_MainMenu");
     }
 }
