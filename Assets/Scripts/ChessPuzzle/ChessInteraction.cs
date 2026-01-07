f56a8cac0fff1b8d6b102f735ef2492e8c6300ad
Karincjansson <Karin.c.jansson@outlook.com>
2026-01-07 12:00:13 +0100

    winning chess
    used Vector3.Lerp to make it clear when player won the chessgame

--- Assets/Scripts/ChessPuzzle/ChessInteraction.cs
+++ Assets/Scripts/ChessPuzzle/ChessInteraction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
@@ -11,6 +12,7 @@ public class ChessInteraction : InteractableBase
     [SerializeField] private List<GameObject> whitePieces;
     [SerializeField] private List<GameObject> whitePiecesThatShouldBeMoved;
     [SerializeField] private List<GameObject> placesWhitePiecesShouldBeMovedTo;
+    [SerializeField] private GameObject blackKing;
 
 
     private bool chessPieceSelected = false;
@@ -28,11 +30,11 @@ public class ChessInteraction : InteractableBase
         whitePiecesWinningPosition = new Dictionary<GameObject, GameObject>();
         whitePiecesCurrentPosition = new Dictionary<GameObject, GameObject>();
         itemShouldBeCameraLocked = true;
-        for (int i = 0; i < whitePieces.Count; i++) 
+        for (int i = 0; i < whitePieces.Count; i++)
         {
             whitePiecesCurrentPosition[whitePieces[i]] = whitePieces[i].transform.parent.gameObject;
 
-            for (int j = 0; j <  whitePiecesThatShouldBeMoved.Count; j++)
+            for (int j = 0; j < whitePiecesThatShouldBeMoved.Count; j++)
             {
                 if (whitePieces[i] == whitePiecesThatShouldBeMoved[j])
                 {
@@ -114,7 +116,7 @@ public class ChessInteraction : InteractableBase
             Debug.DrawRay(ray.origin, ray.direction * 50f, Color.red, 100f);
             if (Physics.Raycast(ray, out hit, 20f, boardMask))
             {
-                if (hit.collider.transform.childCount > 0 )
+                if (hit.collider.transform.childCount > 0)
                 {
                     return;
                 }
@@ -134,8 +136,60 @@ public class ChessInteraction : InteractableBase
         bool hasCompletedPuzzle = whitePiecesWinningPosition.All(kvp => whitePiecesCurrentPosition.TryGetValue(kvp.Key, out var v) && EqualityComparer<GameObject>.Default.Equals(kvp.Value, v));
         if (hasCompletedPuzzle)
         {
+            StartCoroutine(KingPieceFall());
+           
             ChessPuzzleCompleted?.Invoke(this, EventArgs.Empty);
         }
-     
+
+    }
+
+    /// <summary>
+    /// have the king piece, 
+    /// take its vector into a position/angle
+    /// take the roation and postion you wish to have(hard code that shit)
+    /// have floats that count betweeen time
+    /// add in a float t for ....
+    /// use Lerp to make it fall between the postions
+    /// after while is done set transforms to end pos
+    /// </summary>
+    private IEnumerator KingPieceFall()
+    {
+        float timeElapsed = 0f;
+        float animationTimr = 1f;
+        //for rotation and pos hardcode that sht
+        float yRot = 0f;
+        float xRot = -79.4f;
+        float zRot = -39.7f;
+
+        float xPos = 0.04f;
+        float yPos = 0.08f;
+        float zPos = -0.05f;
+
+
+        Vector3 kingPos = blackKing.transform.localPosition;
+        Quaternion kingRot = blackKing.transform.localRotation;
+
+        Vector3 endPos =new Vector3(xPos,yPos, zPos);
+        Quaternion endRot = Quaternion.Euler(xRot, yRot,zRot);
+
+        while (timeElapsed < animationTimr)
+        {
+
+            timeElapsed += Time.deltaTime;
+            //clamp that shit
+            float t = Mathf.Clamp01(timeElapsed / animationTimr);
+
+            blackKing.transform.localPosition = Vector3.Lerp(kingPos, endPos, t);
+            blackKing.transform.localRotation = Quaternion.Lerp(kingRot, endRot, t);
+
+            yield return null;
+
+        }
+        blackKing.transform.localPosition = endPos;
+        blackKing.transform.localRotation = endRot;
+
+
+
+
     }
 }
